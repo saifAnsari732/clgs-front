a22828944b2b46edb1ea5c2874b7813818625003
saifAnsari732 <ansarisaifuddin732@gmail.com>
2025-07-07 12:52:44 +0530

    first atte

--- src/Util.js
+++ src/Util.js
@@ -1,2 +1,2 @@
-// export const BACK="https://saifuddinansari.onrender.com/api/v1";
-export const BACK="http://localhost:3000/api/v1";
+export const BACK="https://saifuddinansari.onrender.com/api/v1";
+// export const BACK="http://localhost:3000/api/v1";

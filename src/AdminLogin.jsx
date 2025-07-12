fffe37a81c5b9569b57376216a1b127893591104
saifAnsari732 <ansarisaifuddin732@gmail.com>
2025-07-12 17:34:16 +0530

    second

--- src/AdminLogin.jsx
+++ src/AdminLogin.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import toast, { Toaster } from "react-hot-toast";
+import  { Toaster } from "react-hot-toast";
 
 const AdminLogin = () => {
   const [email, setEmail] = useState("");
